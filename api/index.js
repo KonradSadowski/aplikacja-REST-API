f27915b2677d0e6deef0e7e512b5d6c5f38d79cf
KonradSadowski <darnokowsky@gmail.com>
2023-09-02 14:37:37 +0200

    done

--- api/index.js
+++ api/index.js
@@ -1,17 +1,29 @@
 const express = require('express')
 const router = express.Router()
-const ctrlContact = require('../controller')
+const controller = require('../controller')
+const passport = require('../middleware/auth')
+const middleware = require('../middleware/auth')
 
-router.get('/contacts', ctrlContact.get)
+// routes for contacts
+router.get('/contacts', controller.get)
 
-router.get('/contacts/:id', ctrlContact.getById)
+router.get('/contacts/:id', controller.getById)
 
-router.post('/contacts', ctrlContact.create)
+router.post('/contacts', controller.create)
 
-router.put('/contacts/:id', ctrlContact.update)
+router.put('/contacts/:id', controller.update)
 
-router.patch('/contacts/:id/favorite', ctrlContact.updateFavorite)
+router.patch('/contacts/:id/favorite', controller.updateFavorite)
 
-router.delete('/contacts/:id', ctrlContact.remove)
+router.delete('/contacts/:id', controller.remove)
+
+// routes for users
+router.post('/users/signup', controller.signUp)
+
+router.post('/users/login', controller.logIn)
+
+router.get('/users/logout', middleware, controller.logOut)
+
+router.get('/users/current', middleware, controller.currentUser)
 
 module.exports = router
